73a92711148460abc3662007e2826ec6185ad9ab
Karthick <ngk@btlnet.com>
2018-05-14 15:14:59 +0530

    Initial commit of all files in notes-app

---
+++ notes-app-api/delete.js
@@ -0,0 +1,25 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+import AWS from "aws-sdk"
+
+AWS.config.update({region: "ap-southeast-1"});
+
+export async function main(event, context, callback) {
+  const params = {
+    TableName: "notes",
+    // 'Key' defines the partition key and sort key of the item to be removed
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userid: event.requestContext.identity.cognitoIdentityId,
+      noteid: event.pathParameters.id
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("delete", params);
+    callback(null, success({ status: true }));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
